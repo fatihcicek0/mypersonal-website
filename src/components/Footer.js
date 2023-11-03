36ddb2221fd40d5c23888a2db02b6c22a253e65d
Fatih <fatihcicek896@gmail.com>
2023-11-03 20:06:33 +0300

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer(){
     return (
         <div>
-            <div style={{height:100,backgroundColor:'#f9f7f7'}}>
+            <div style={{height:100,backgroundColor:'#f9f7f7',borderTop:'1px solid rgb(235, 233, 233)'}}>
                 <p style={{textAlign:'center',color:'lightgray',paddingTop:40}}>© 2023 cfatih.dev | Tüm Hakları saklıdır</p>
             </div>
             <div style={{height:10,backgroundColor:'gray'}}></div>
