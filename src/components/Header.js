075ecae0911c20b061ad81a0fecdfbebb051a083
Fatih <fatihcicek896@gmail.com>
2023-11-03 14:11:12 +0300

    add burger menu feat

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,23 @@
 import { Link, NavLink } from 'react-router-dom';
 export default function Header() {
+    const clickBurger = () => {
+        const burger = document.querySelector('#burger-menu');
+        const nav = document.querySelector('.nav');
+        burger.classList.toggle('close');
+        nav.classList.toggle('open');
+    }
     return (
         <header>
             <Link id='name'>CFatih.developer 👋</Link>
-            <nav>
+            <nav className='nav'>
                 <NavLink to="/">Anasayfa</NavLink>
                 <a to="#solution">Hizmetler</a>
             </nav>
+            <div id="burger-menu" onClick={() => clickBurger()}>
+                <div className="line" id='line1'></div>
+                <div className="line" id='line2'></div>
+                <div className="line" id='line3'></div>
+            </div>
         </header>
     )
 }
